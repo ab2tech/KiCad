20ad2b362cfbea482424c01c669d4992239b653e
Austin Beam <austinbeam@gmail.com>
2014-10-27 23:12:37 -0500

    scripts: blender_wrl_to_x3d: Delete that stupid cube
    Blender puts a default cube in place in the default scene, but we don't
    want this exported in our X3D models, so we need to delete it before
    doing anything else.

--- scripts/blender_wrl_to_x3d.py
+++ scripts/blender_wrl_to_x3d.py
@@ -13,5 +13,7 @@ argv = argv[argv.index("--") + 1:] # get all args after "--"
 wrl_in = argv[0]
 x3d_out = argv[1]
 
+# Delete that stupid cube
+bpy.ops.object.delete()
 bpy.ops.import_scene.x3d(filepath=wrl_in)
 bpy.ops.export_scene.x3d(check_existing=False, filepath=x3d_out)
